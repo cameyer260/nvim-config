5a1a0e26eaf359aecae49c55571486383a07995d
Christopher Meyer <cameyer06@gmail.com>
2025-11-05 08:58:47 -0600

    added remove command for harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -30,6 +30,7 @@ return {
     config = function()
         local harpoon = require('harpoon')
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+        vim.keymap.set("n", "<leader>aa", function() harpoon:list():remove() end)
         vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
         vim.keymap.set("n", "<leader>fl", function() toggle_telescope(harpoon:list()) end,
             { desc = "Open harpoon window" })
