3ff5722d9dbb26c33e086c655812d17eecd14b84
Christopher Meyer <cameyer06@gmail.com>
2025-11-06 11:28:05 -0600

    added prisma files to have tablength of 2 in the options file

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,7 +10,7 @@ vim.cmd("filetype indent off") -- turn off default auto indent nvim uses
 
 -- Set indentation to width of 2 for web dev files
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "json" },
+    pattern = { "javascript", "typescript", "javascriptreact", "typescriptreact", "html", "json", "prisma" },
     callback = function()
         vim.opt_local.tabstop = 2
         vim.opt_local.shiftwidth = 2
